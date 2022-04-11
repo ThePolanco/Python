c1103a100233176c27cf396bb6797ec79549d9bc
Nocturnal08 <nocturnal1721@gmail.com>
2022-04-11 12:30:52 -0500

    Create program1.py

---
+++ Only me/program1.py
@@ -0,0 +1,18 @@
+""" print("Hola Mundo") 
+print(1+3)
+print(2*5)
+print(-1/2)
+print(2**5) 
+
+nombre = "Nocturnal"
+print ("Hola Mundo, soy " + nombre)
+
+Año = int(2022)
+Año_sig = Año+1
+print("El año siguiente sera:")
+print (Año_sig)
+
+def Saludo(Nombre):
+    return ("Hola " + Nombre)
+Saludo_1 = Saludo("Nocturnal")
+print(Saludo_1) """
