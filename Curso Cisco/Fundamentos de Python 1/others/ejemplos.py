5168a10cd990df614a951ac405dc2fe4d7459aa3
ThePolanco <polancojeison21@gmail.com>
2024-01-03 18:49:59 -0500

    Ejemplos de practica en Python

---
+++ Curso Cisco/Fundamentos de Python 1/others/ejemplos.py
@@ -0,0 +1,29 @@
+#Ejercicios de practica de la pagina web https://aprendeconalf.es/docencia/python
+# 4.
+"""
+operacion = ((3+2)/(2*5))**2
+print (operacion)
+"""
+# 5.
+"""
+h_trabajadas = float(input("Digita el numero de horas trabajadas: "))
+c_hora = float(input("Digita el coste por hora: "))
+
+paga = h_trabajadas * c_hora
+print ("Su paga es: " , paga)
+"""
+
+# 6. 
+"""
+n = int(input("Introduce un número entero: "))
+suma = n * (n + 1) / 2
+print("La suma de los primeros números enteros desde 1 hasta " + str(n) + " es " + str(suma))
+"""
+
+# 7. 
+"""
+peso = float(input("Introduce el peso en kg: "))
+estatura = float(input("Introduce la estatura en metros: "))
+imc = round(peso / estatura ** 2,2)
+print ("El indice de masa corporal es: " , imc)
+"""
