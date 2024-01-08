38fa3010c63d544db427efaf75d4c2fd5ad07a00
ThePolanco <polancojeison21@gmail.com>
2024-01-08 16:29:37 -0500

    Create try_except

---
+++ Curso Cisco/Fundamentos de Python 1/Excepciones/try_except.py
@@ -0,0 +1,10 @@
+try:
+    value = int(input('Ingresa un número natural: '))
+    reciprocal = 1 / value
+    print('El recíproco de', value, 'es', reciprocal)
+except ValueError:
+    print('Error: Debes ingresar un número válido.')
+except ZeroDivisionError:
+    print('Error: No se puede calcular el recíproco de 0.')
+except Exception as e:
+    print('Ocurrió un error no manejado:', e)
